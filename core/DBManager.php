2f844e1e7972a6143809b577b5d8376bef274703
natsume0718 <moto718program@gmail.com>
2019-08-13 17:13:58 +0900

    DB接続と設定クラス作成

---
+++ core/DBManager.php
@@ -0,0 +1,116 @@
+<?php
+
+class DBManager
+{
+    protected $connections = [];
+
+    protected $repository_connection_map = [];
+
+    protected $repositories = [];
+
+    /**
+     * 接続情報からPDOインスタンス生成を行う
+     *
+     * @param  string $name
+     * @param  array $params
+     *
+     */
+    public function connect(string $name, array $params)
+    {
+        $params = array_merge([
+            'dsn' => null,
+            'user' => '',
+            'password' => '',
+            'options' => [],
+        ], $params);
+
+        $connect = new PDO(
+            $params['dsn'],
+            $params['uer'],
+            $params['password'],
+            $params['options']
+        );
+
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connections[$name] = $connect;
+    }
+
+    /**
+     * 接続したconnectionを取得する
+     *
+     * @param  string $name
+     *
+     * @return array
+     */
+    public function getConnection(string $name = null)
+    {
+        if (!$name) {
+            return current($this->connections);
+        }
+
+        return $this->connections[$name];
+    }
+
+    /**
+     * Repositoryに接続情報をセットする
+     *
+     * @param  string $repository_name
+     * @param  string $name
+     *
+     */
+    public function setRepositoryConnection(string $repository_name, string $name)
+    {
+        $this->repository_connection_map[$repository_name] = $name;
+    }
+
+    /**
+     * リポジトリに対応する接続情報を取得する
+     *
+     * @param  string $repository_name
+     *
+     */
+    public function getRepositoryConnection(string $repository_name)
+    {
+        $connection = [];
+        if (isset($this->repository_connection_map[$repository_name])) {
+            $name = $this->repository_connection_map[$repository_name];
+            $connection = $this->getConnection($name);
+        } else {
+            $connection = $this->getConnection();
+        }
+        return $connection;
+    }
+
+    /**
+     * Repositoryのインスタンスを取得する
+     *
+     * @param  string $repository_name
+     *
+     */
+    public function get(string $repository_name)
+    {
+        if (!isset($this->repository_connection_map[$repository_name])) {
+            $repository_class = $repository_name . 'Repository';
+            $connect = $this->getRepositoryConnection($repository_name);
+
+            $repository = new $repository_class($connect);
+            $this->repositories[$repository_name] = $repository_class;
+        }
+        return $this->repositories['$repository_name'];
+    }
+
+    /**
+     * 接続情報の破棄
+     * 
+     */
+    public function __destruct()
+    {
+        foreach ($this->repositories as $repository) {
+            unset($repository);
+        }
+
+        foreach ($this->connections as $connection) {
+            unset($connection);
+        }
+    }
+}
---
+++ core/DBRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class DBRepository
+{
+    protected $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function execute(string $sql, array $params = [])
+    {
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt;
+    }
+
+    /**
+     * 結果を一行取得する
+     *
+     * @param  string $sql
+     * @param  array $params
+     *
+     * @return array
+     */
+    public function fetch(string $sql, array $params = [])
+    {
+        return $this->execute($sql, $params)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * 結果をすべて取得する
+     *
+     * @param  string $sql
+     * @param  array $params
+     *
+     * @return array
+     */
+    public function fetchAll(string $sql, array $params = [])
+    {
+        return $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
